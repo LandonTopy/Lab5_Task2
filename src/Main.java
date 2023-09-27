3d4348b74eb0ab12b10ec94c59decb7afbde88c6
LandonTopy <landontopy@gmail.com>
2023-09-27 09:21:45 -0400

    y

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int birthMonth;
+
+        System.out.println("What is your birth month? (Using 1-12)");
+        if (scan.hasNextInt()) {
+            birthMonth = scan.nextInt();
+            if (1 <= birthMonth && 12 >= birthMonth) {
+                System.out.println("Your birth month is " + birthMonth);
+            } else
+                System.out.println("The number you entered is not between 1 and 12");
+        }
+        else
+            System.out.println("The value you entered is incorrect");
     }
-}
+    }
